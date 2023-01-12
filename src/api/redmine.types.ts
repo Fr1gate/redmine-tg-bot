f94654a7f623047f2dc5c9f9e7dcf5f182cb48a2
Ruslan MR <mfear13@gmail.com>
2023-01-12 19:43:01 +0300

    Fix eslint

--- src/api/redmine.types.ts
+++ src/api/redmine.types.ts
@@ -1,84 +1,82 @@
-import {DateLike} from "../globals";
-
 export namespace RedmineAPITypes {
-    export interface IssuesResponse {
-        issues: Issue[]
-    }
+  export interface IssuesResponse {
+    issues: Issue[];
+  }
 
-    export interface Issue {
-        "id": number,
-        "project": {
-            "id": number,
-            "name": string
-        },
-        "tracker": {
-            "id": number,
-            "name": string
-        },
-        "status": {
-            "id": number,
-            "name": string
-        },
-        "priority": {
-            "id": number,
-            "name": string
-        },
-        "author": {
-            "id": number,
-            "name": string
-        },
-        "assigned_to": {
-            "id": number,
-            "name": string
-        },
-        "category": {
-            "id": number,
-            "name": string
-        },
-        "fixed_version": {
-            "id": number,
-            "name": string
-        },
-        "parent": {
-            "id": number
-        },
-        "subject": string,
-        "description": string,
-        "start_date": string,
-        "due_date": null | string,
-        "done_ratio": number,
-        "is_private": boolean,
-        "estimated_hours": null | number,
-        "created_on": string,
-        "updated_on": string,
-        "closed_on": null | string
-    }
+  export interface Issue {
+    id: number;
+    project: {
+      id: number;
+      name: string;
+    };
+    tracker: {
+      id: number;
+      name: string;
+    };
+    status: {
+      id: number;
+      name: string;
+    };
+    priority: {
+      id: number;
+      name: string;
+    };
+    author: {
+      id: number;
+      name: string;
+    };
+    assigned_to: {
+      id: number;
+      name: string;
+    };
+    category: {
+      id: number;
+      name: string;
+    };
+    fixed_version: {
+      id: number;
+      name: string;
+    };
+    parent: {
+      id: number;
+    };
+    subject: string;
+    description: string;
+    start_date: string;
+    due_date: null | string;
+    done_ratio: number;
+    is_private: boolean;
+    estimated_hours: null | number;
+    created_on: string;
+    updated_on: string;
+    closed_on: null | string;
+  }
 
-    export interface TimeEntriesResponse {
-        time_entries: Array<TimeEntry>
-    }
+  export interface TimeEntriesResponse {
+    time_entries: TimeEntry[];
+  }
 
-    export interface TimeEntry {
-        "id": number,
-        "project": {
-            "id": number,
-            "name": string
-        },
-        "issue": {
-            "id": number
-        },
-        "user": {
-            "id": number,
-            "name": string
-        },
-        "activity": {
-            "id": number,
-            "name": string
-        },
-        hours: number;
-        "comments": string;
-        "spent_on": string;
-        "created_on": string;
-        "updated_on": string;
-    }
+  export interface TimeEntry {
+    id: number;
+    project: {
+      id: number;
+      name: string;
+    };
+    issue: {
+      id: number;
+    };
+    user: {
+      id: number;
+      name: string;
+    };
+    activity: {
+      id: number;
+      name: string;
+    };
+    hours: number;
+    comments: string;
+    spent_on: string;
+    created_on: string;
+    updated_on: string;
+  }
 }
